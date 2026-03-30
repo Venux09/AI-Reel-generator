8174f9d38dd799df540e71e7b160d048873c3f12
monu242 <monucut657@gmail.com>
2026-03-30 14:42:08 +0530

    add: writed he comments in generation  process

--- generate_process.py
+++ generate_process.py
@@ -6,29 +6,29 @@ import subprocess
 
 
 def text_to_audio(folder):
-    print("TTA - ", folder)
-    with open(f"user_uploads/{folder}/desc.txt") as f:
+    print("TTA - ", folder)#print the folder name 
+    with open(f"user_uploads/{folder}/desc.txt") as f:#to read the text user has give in the desc.txt
         text = f.read()
     print(text, folder)
     text_to_speech_file(text, folder)
 
-def create_reel(folder):
+def create_reel(folder):#func for creating the reel using input.txt and audio.mp4 then ffmpeg do the work
     command = f'''ffmpeg -f concat -safe 0 -i user_uploads/{folder}/input.txt -i user_uploads/{folder}/audio.mp3 -vf "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2:black" -c:v libx264 -c:a aac -shortest -r 30 -pix_fmt yuv420p static/reels/{folder}.mp4'''
-    subprocess.run(command, shell=True, check=True)
+    subprocess.run(command, shell=True, check=True)#subprocess help in running the command in the terminal 
     
     print("CR - ", folder)
 
 if __name__ == "__main__":
     while True:
         print("Processing queue...")
-        with open("done.txt", "r") as f:
+        with open("done.txt", "r") as f:#opens the done.txt which we have created to save the useruploads folder path or filename
             done_folders = f.readlines()
 
         done_folders = [f.strip() for f in done_folders]
         folders = os.listdir("user_uploads") 
         for folder in folders:
             if(folder not in done_folders): 
-                text_to_audio(folder) # Generate the audio.mp3 from desc.txt
+                text_to_audio(folder) 
                 create_reel(folder) # Convert the images and audio.mp3 inside the folder to a reel
                 with open("done.txt", "a") as f:
                     f.write(folder + "\n")
